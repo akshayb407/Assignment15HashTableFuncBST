1813f369d8aef9fc803222e85df1b7ac05c7b391
akshayb407 <akshayb407@gmail.com>
2022-08-16 18:29:40 +0530

    UC1BST

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HashTable
 {
@@ -7,99 +10,14 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please choose the option :\n1)Frequency of a sentence\n" +
-             "2)Frequency of a Paragraph\n3)Removing words");
-            int option = Convert.ToInt16(Console.ReadLine());
-            switch (option)
-            {
-                case 1:
-                    HashTableMethod<string, int> hash = new HashTableMethod<string, int>(5);
-                    string Words = "To be or not to be";
-                    string[] array = Words.Split(' ');
-                    LinkedList<string> checkForDuplicationS = new LinkedList<string>();
-                    foreach (string element in array)
-                    {
-                        int count = 0;
-                        foreach (string match in array)
-                        {
-                            if (element == match)
-                            {
-                                count++;
-                                if (checkForDuplicationS.Contains(element))
-                                {
-                                    break;
-                                }
-                            }
-                        }
-                        if (!checkForDuplicationS.Contains(element))
-                        {
-                            checkForDuplicationS.AddLast(element);
-                            hash.Add(element, count);
-                        }
-                    }
-                    hash.Display();
-                    break;
+            Tree<int> tree = new Tree<int>(3);
+            tree.Insert(56);
+            tree.Insert(70);
+            tree.Insert(30);
+            tree.Display();
 
-                case 2:
-                    HashTableMethod<string, int> hash2 = new HashTableMethod<string, int>(5);
-                    string Word = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-                    string[] array2 = Word.Split(' ');
-                    LinkedList<string> checkForDuplication2 = new LinkedList<string>();
-                    foreach (string element in array2)
-                    {
-                        int count = 0;
-                        foreach (string match in array2)
-                        {
-                            if (element == match)
-                            {
-                                count++;
-                                if (checkForDuplication2.Contains(element))
-                                {
-                                    break;
-                                }
-                            }
-                        }
-                        if (!checkForDuplication2.Contains(element))
-                        {
-                            checkForDuplication2.AddLast(element);
-                            hash2.Add(element, count);
-                        }
-                    }
-                    hash2.Display();
-                    break;
-                case 3:
-                    HashTableMethod<string, int> hash3 = new HashTableMethod<string, int>(5);
-                    string Para = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-                    string[] arr = Para.Split(' ');
-                    LinkedList<string> checkForduplication = new LinkedList<string>();
-                    foreach (string element in arr)
-                    {
-                        int count = 0;
-                        foreach (string match in arr)
-                        {
-                            if (element == match)
-                            {
-                                count++;
-                                if (checkForduplication.Contains(element))
-                                {
-                                    break;
-                                }
-                            }
-                        }
-                        if (!checkForduplication.Contains(element))
-                        {
-                            checkForduplication.AddLast(element);
-                            hash3.Add(element, count);
-                        }
-                    }
-                    hash3.Display();
-                    Console.WriteLine("Please enter the word u want to remove :");
-                    string WordToRemove = Console.ReadLine();
-                    hash3.Remove(WordToRemove);
-                    hash3.Display();
-                    break;
-            }
-
+            
+            
         }
     }
 }
---
+++ HashTable/Tree.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    internal class Tree<T>where T :IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public Tree<T> LeftTree { get; set; }
+        public Tree<T> RightTree { get; set; } 
+        public Tree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = LeftTree;
+            this.RightTree = RightTree;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if (currentNodeValue.CompareTo(item) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new Tree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new Tree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+
+            }
+
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+
+            Console.WriteLine(this.NodeData.ToString());
+        }
+        public void GetSize()
+        {
+            //Display(binarytree);
+            Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+        }
+        public bool IfExists(T element, Tree<T> node)
+        {
+            if (node == null)
+            {
+                result = false;
+            }
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST" + " " + node.NodeData);
+                result = true;
+            }
+            else
+            {
+                Console.WriteLine("Current element is {0} in BST", node.NodeData);
+            }
+            if (element.CompareTo(node.NodeData) < 0)
+            {
+                IfExists(element, node.LeftTree);
+            }
+            if (element.CompareTo(node.NodeData) > 0)
+            {
+                IfExists(element, node.RightTree);
+            }
+            return result;
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
