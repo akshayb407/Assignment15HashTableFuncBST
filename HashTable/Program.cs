1918d7a85af6f458c321f233d2b2f814ab0b1c4c
akshayb407 <akshayb407@gmail.com>
2022-08-16 13:10:32 +0530

    UC1HashTable

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace HashTable
 {
     internal class Program
@@ -7,6 +7,40 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Table Progrma!");
+            Console.WriteLine("Please choose the option :\n1)Frequency of a sentence\n");
+            int option = Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    HashTableMethod<string, int> hash = new HashTableMethod<string, int>(5);
+                    string Words = "To be or not to be";
+                    string[] array = Words.Split(' ');
+                    LinkedList<string> checkForDuplicationS = new LinkedList<string>();
+                    foreach (string element in array)
+                    {
+                        int count = 0;
+                        foreach (string match in array)
+                        {
+                            if (element == match)
+                            {
+                                count++;
+                                if (checkForDuplicationS.Contains(element))
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        if (!checkForDuplicationS.Contains(element))
+                        {
+                            checkForDuplicationS.AddLast(element);
+                            hash.Add(element, count);
+                        }
+                    }
+                    hash.Display();
+                    break;
+
+
+            }
         }
     }
 }
